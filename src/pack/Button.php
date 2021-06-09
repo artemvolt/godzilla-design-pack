4d3c739f502e1b76dbe4dd68dd57699a3cdd11f3
Artem <artem@artwake.ru>
2021-06-09 22:22:08 +0300

    Examples.

---
+++ src/pack/Button.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Godzilla\Pack;
+
+/**
+ * Interface Button
+ * @package Godzilla\Pack
+ */
+interface Button
+{
+    /**
+     * @return string
+     */
+    public function display() : string;
+}
