25dd9a8417a11f65e70d9a6f11f1b152e63d0325
Artem <artem@artwake.ru>
2021-08-09 17:09:13 +0300

    Unit test.

---
+++ tests/unit/pack/DesignPackTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace pack;
+
+use Godzilla\Pack\DesignPack;
+use Godzilla\Pack\Icon;
+
+class DesignPackTest extends \Codeception\Test\Unit
+{
+    protected $pack;
+
+    protected function _before()
+    {
+        $this->pack = new DesignPack('bootstrap');
+    }
+
+    protected function _after()
+    {
+        $this->pack = null;
+    }
+
+    // tests
+    public function testButtonBootstrap()
+    {
+        $this->assertEquals(
+            $expected = "<button class='btn btn-primary'><i class='fa fa-icon bolt'></i>Hello</button>",
+            $this->pack->button('Hello')
+                ->withIcon(Icon::POSITION_LEFT, ['font-pack' => 'fa', 'icon' => 'bolt'])
+                ->display()
+        );
+        $this->assertEquals(
+            $expected,
+            $this->pack->button('Hello')
+                ->withLeftIcon(['font-pack' => 'fa', 'icon' => 'bolt'])
+                ->display()
+        );
+    }
+}
